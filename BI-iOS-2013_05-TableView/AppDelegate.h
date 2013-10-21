0ee13582c9844ba409bbbaa17557998df18b1188
Jakub Hladik <kubbing@me.com>
2013-10-21 06:33:01 +0200

    Initial Commit

---
+++ BI-iOS-2013_05-TableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BI-iOS-2013_05-TableView
+//
+//  Created by Jakub Hladík on 21.10.13.
+//  Copyright (c) 2013 Flowknight s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BI-iOS-2013_05-TableViewTests/BI_iOS_2013_05_TableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  BI_iOS_2013_05_TableViewTests.m
+//  BI-iOS-2013_05-TableViewTests
+//
+//  Created by Jakub Hladík on 21.10.13.
+//  Copyright (c) 2013 Flowknight s.r.o. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BI_iOS_2013_05_TableViewTests : XCTestCase
+
+@end
+
+@implementation BI_iOS_2013_05_TableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
